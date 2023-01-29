186464911d006711e3084d01c841ed771f2a07d1
Julianne <juliaguas17@gmail.com>
2023-01-28 23:17:25 -0600

    added API calls to js and svgs to assets/icons

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,25 @@
-// store var API Key
-// create variables for the API to call
-// construct Query URL to Make the API call
-// make the API call using fetch()
+// use day.js to pull current date in MM/DD/YYYY format
 
+
+// store var API Key
+var key = '2b82885f735995470d3e4c3561b9e55c';
+// create variables for the API to call
+var city;
 
 // Built-in API request by city name
-// api.openweathermap.org/data/2.5/forecast?q={city name}&appid={API key}
+let api;
+function requestAPI (city) {
+    // construct Query URL to Make the API call
+    queryURL = 'https://api.openweathermap.org/data/2.5/weather?q=$'+ city +'&appid='+ key;
+    // make the API call using fetch()
+    fetch(queryURL);
+};
 
-// Switch to metric units
+
+// Weather Parameters to Pull:
+// Temperature
+// Wind Speed
+// Humidity
+
+// Switch to metric units (OPTIONAL):
 // api.openweathermap.org/data/2.5/forecast?lat=57&lon=-2.15&appid={API key}&units=metric
