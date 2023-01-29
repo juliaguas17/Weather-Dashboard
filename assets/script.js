6550135a90d8b94a7f282e439a91946be29f9721
Julianne <juliaguas17@gmail.com>
2023-01-28 22:47:50 -0600

    added js pseudocode to make API calls

---
+++ assets/script.js
@@ -0,0 +1,11 @@
+// store var API Key
+// create variables for the API to call
+// construct Query URL to Make the API call
+// make the API call using fetch()
+
+
+// Built-in API request by city name
+// api.openweathermap.org/data/2.5/forecast?q={city name}&appid={API key}
+
+// Switch to metric units
+// api.openweathermap.org/data/2.5/forecast?lat=57&lon=-2.15&appid={API key}&units=metric
